fd2a6b9be2b4ae51353fbab53c45ef5978123da7
whs_c207_macbook <whs_c207_macbook@whs-c207-macbooks-MacBook-Air.local>
2017-03-13 10:23:46 -0400

    Initial Commit

---
+++ AthleticArchive/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AthleticArchive
+//
+//  Created by whs_c207_macbook on 3/13/17.
+//  Copyright © 2017 AthleticArchive. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
